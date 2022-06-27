8dc6ed4c8620fd01a4cb35f5e6925a4e831208b7
brunomaria71 <brunomaria71@yahoo.com>
2022-06-27 19:50:38 -0400

    added table

--- src/components/ToDoApp.vue
+++ src/components/ToDoApp.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>Hello World</div>
+  <div class="container">
+    <h2 class="text-center mt-5">To Do List</h2>
+
+    <!--Input-->
+    <div class="d-flex">
+      <input type="text" placeholder="Enter task" class="form-control" />
+      <button class="btn btn-warning rounded-0">Submit</button>
+    </div>
+    <!--Task table -->
+    <table class="table table-bordered mt-5">
+      <thead>
+        <tr>
+          <th scope="col">Task</th>
+          <th scope="col">Status</th>
+          <th scope="col" class="text-center">#</th>
+          <th scope="col" class="text-center">#</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>Pressure wash the car</th>
+          <td>To-Do</td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
