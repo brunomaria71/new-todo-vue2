14b6582a074bcb0e08520e5ad4643bb01a7b35cd
brunomaria71 <brunomaria71@yahoo.com>
2022-06-27 22:13:01 -0400

    added some styling

--- src/components/ToDoApp.vue
+++ src/components/ToDoApp.vue
@@ -1,54 +1,56 @@
 <template>
-  <div class="container">
-    <h2 class="text-center mt-5">To Do List</h2>
+  <div>
+    <div class="container">
+      <h2 class="text-center mt-5">To Do List</h2>
 
-    <!--Input box for a new task-->
-    <div class="d-flex">
-      <input
-        v-model="task"
-        type="text"
-        placeholder="Enter task"
-        class="form-control"
-      />
-      <button @click="submitTask" class="btn btn-info rounded-0 color-purple">
-        Submit
-      </button>
+      <!--Input box for a new task-->
+      <div class="d-flex">
+        <input
+          v-model="task"
+          type="text"
+          placeholder="Enter task"
+          class="form-control"
+        />
+        <button @click="submitTask" class="btn btn-info rounded-0 color-purple">
+          Submit
+        </button>
+      </div>
+      <!--Task table -->
+      <table class="table table-bordered mt-5">
+        <thead>
+          <tr>
+            <th scope="col">Task</th>
+            <th scope="col">Status</th>
+            <th scope="col" class="text-center">Edit</th>
+            <th scope="col" class="text-center">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(task, index) in tasks" :key="index">
+            <th>
+              <span :class="{ completed: task.status === 'completed' }">{{
+                task.name
+              }}</span>
+            </th>
+            <td>
+              <span @click="changeStatus(index)" role="button">{{
+                task.status
+              }}</span>
+            </td>
+            <td>
+              <div class="text-center" @click="editTask(index)">
+                <span class="fa fa-pen"></span>
+              </div>
+            </td>
+            <td>
+              <div class="text-center" @click="deleteTask(index)">
+                <span class="fa fa-trash"></span>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-    <!--Task table -->
-    <table class="table table-bordered mt-5">
-      <thead>
-        <tr>
-          <th scope="col">Task</th>
-          <th scope="col">Status</th>
-          <th scope="col" class="text-center">Edit</th>
-          <th scope="col" class="text-center">Delete</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(task, index) in tasks" :key="index">
-          <th>
-            <span :class="{ completed: task.status === 'completed' }">{{
-              task.name
-            }}</span>
-          </th>
-          <td>
-            <span @click="changeStatus(index)" role="button">{{
-              task.status
-            }}</span>
-          </td>
-          <td>
-            <div class="text-center" @click="editTask(index)">
-              <span class="fa fa-pen"></span>
-            </div>
-          </td>
-          <td>
-            <div class="text-center" @click="deleteTask(index)">
-              <span class="fa fa-trash"></span>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
   </div>
 </template>
 
@@ -111,5 +113,16 @@ export default {
 .completed {
   text-decoration-line: line-through;
   color: green;
+}
+
+.bg {
+  background: -webkit-linear-gradient(70deg, #fff810 30%, rgba(0, 0, 0, 0) 30%),
+    -webkit-linear-gradient(30deg, #63e89e 60%, #ff7ee3 60%);
+  background: -o-linear-gradient(70deg, #fff810 30%, rgba(0, 0, 0, 0) 30%),
+    -o-linear-gradient(30deg, #63e89e 60%, #ff7ee3 60%);
+  background: -moz-linear-gradient(70deg, #fff810 30%, rgba(0, 0, 0, 0) 30%),
+    -moz-linear-gradient(30deg, #63e89e 60%, #ff7ee3 60%);
+  background: linear-gradient(70deg, #fff810 30%, rgba(0, 0, 0, 0) 30%),
+    linear-gradient(30deg, #63e89e 60%, #ff7ee3 60%);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import "@fortawesome/fontawesome-free/js/all.js";
 import VueCompositionAPI, { createApp, h } from "@vue/composition-api";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Vue from "vue";
+import "./styles/styles.css";
 
 import App from "./App.vue";
 
